0e50ddd2ddfb94844ff3c7b5be6cdd22f1ec071b
Joonathan Anjam <AG1661@student.jamk.fi>
2025-04-30 13:14:17 +0300

    buttonien border tyylitys eri stateissa, navigaation button state tyylitys

--- project/src/lib/components/Nav.svelte
+++ project/src/lib/components/Nav.svelte
@@ -5,8 +5,10 @@
 <header>
 	<nav class="grid grid-cols-2 bg-black p-4 place-items-center">
 		{#key page.url.pathname}
-			<a class="text-shadow-valkoinen items-center font-['Rosarivo'] text-amber-50 transition duration-175 ease-in-out hover:scale-101 rounded-xl border-4 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#e2e2e2]" href="/">Kotisivu</a>
-			<a class="text-shadow-valkoinen items-center font-['Rosarivo'] text-amber-50 transition duration-175 ease-in-out hover:scale-101 rounded-xl border-4 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#e2e2e2]" href="/gallery">Galleria</a>
+			<a class="text-shadow-valkoinen text-2xl items-center font-['Rosarivo'] text-amber-50 transition duration-175 ease-in-out hover:scale-101 rounded-full border-4 border-black px-5 py-2 focus:outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700] active:bg-gray-900" href="/">
+        Kotisivu</a>
+			<a class="text-shadow-valkoinen text-2xl items-center font-['Rosarivo'] text-amber-50 transition duration-175 ease-in-out hover:scale-101 rounded-full border-4 border-black px-5 py-2 focus:outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700] active:bg-gray-900" href="/gallery">
+        Galleria</a>
 		{/key}
 	</nav>
 </header>
