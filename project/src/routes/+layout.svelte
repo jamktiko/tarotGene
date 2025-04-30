634d062256ec0d26153a4347aa0f7b6b795a88ce
Thomas, Vainikainen <ag2982@student.jamk.fi>
2025-04-30 11:33:36 +0300

    Footer lisätty

--- project/src/routes/+layout.svelte
+++ project/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from '$lib/components/Nav.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	let { children } = $props();
 </script>
 
 <Header></Header>
 
 {@render children()}
+
+<Footer></Footer>
