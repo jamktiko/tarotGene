fbb0aba3a420ff838defe7e993d27fb46ef421c6
Thomas, Vainikainen <ag2982@student.jamk.fi>
2025-05-06 10:16:28 +0300

    rakkaus desc lisätty jsoniin

---
+++ project/src/lib/components/AiheVal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  
+let aihe=[{nimi:"rakkaus"},
+  {nimi:"tulevaisuus"},
+  {nimi:"koulu"}
+]
+let valittuAihe=$state(0)
+
+</script>
+
+<select bind:value={valittuAihe}>
+  <option value={0}>Rakkaus</option>
+  <option value={1}>Tulevaisuus</option>
+  <option value={2}>Koulu</option>
+</select>
