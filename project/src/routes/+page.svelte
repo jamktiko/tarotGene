b6d48f1aeed3a72751ea5d9b3194e2ebb9118620
Joonathan Anjam <AG1661@student.jamk.fi>
2025-05-06 11:45:08 +0300

    etusivu layout fixattu ja responsiivinen

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -83,43 +83,48 @@ nostetut.tyhjenna()
 		{#if !fiftyFifty.booleani}
 
 			<!-- Alkusivu -->
-			<div class="gap m-6 grid grid-cols-3 items-center">
+			<div class="flex items-center justify-center gap-6 p-4 flex-row sm:gap-10">
 
-        <div>
+        <div class="w-16 sm:w-20">
 				<Button onclick={() => fiftyFifty.maara--} text="/images/minus.png" disabled={fiftyFifty.maara <= 0} />
         </div>
 
           <!--BOUNCY KORTIT-->
 
           <!--Blurrattu tausta kortti-->
-          <div>
-          <div class=" flex h-90">
+          <div class="relative w-40 sm:w-48 md:w-60 aspect-[2/3]">
+
+          
           <img
           src="cardBack.png"
-          class="mx-auto flex h-90 w-60 top-0 right-0 left-0 bottom-0 blur-[2px] rounded-xl border-2 border-black outline-1 outline-[#FFD700]"
+          class="absolute left-1/2 top-0 h-full w-full blur-[2px] rounded-xl border-4 border-black outline-1 outline-[#FFD700] z-0 transform -translate-x-1/2 object-contain"
           alt="Kortti"
 					onclick={kortinNaytto}
        transition:fade />
+    
+
 
 
        <!--Eka kortti-->
+       <div class="z-10">
 				{#if fiftyFifty.maara>0}
-		
 				<img
 				src="cardBack.png"
-				class="mx-auto flex h-90 w-60 -translate-x-[100%] motion-safe:animate-[bounce_5s_infinite] transition duration-175 ease-in-out hover:scale-101 rounded-xl border-2 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700]"
+				class="absolute left-1/2 top-0 rounded-xl border-4 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700] z-10 transform -translate-x-1/2 transition duration-300 ease-in-out hover:scale-101 object-contain motion-safe:animate-[bounce_5s_infinite]"
 				alt="Kortti"
 				onclick={kortinNaytto}
 
 		in:fade
 			out:fly|global={{ x: 0, y: -1500 ,  duration: 1000}}/>
 				{/if}
+      </div>
 
        <!--Toka kortti-->
+       <div class="z-10">
 				{#if fiftyFifty.maara>1}
 				<img
 				src="cardBack.png"
-				class="mx-auto flex h-90 w-60 -translate-x-[200%] motion-safe:animate-[bounce_5s_infinite] transition duration-175 ease-in-out hover:scale-101 rounded-xl border-2 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700]"
+				class="absolute left-1/2 top-0 rounded-xl border-4 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700] z-20 transform -translate-x-1/2 transition duration-300 ease-in-out hover:scale-101 object-contain motion-safe:animate-[bounce_5s_infinite]"
 				alt="Kortti"
 				onclick={kortinNaytto}
 
@@ -127,12 +132,14 @@ nostetut.tyhjenna()
 				out:fly|global={{ x: 0, y: -1500 ,  duration: 1000, delay:100}}
 			/>
 				{/if}
+      </div>
 
        <!--Kolmas kortti-->
+       <div class="z-10">
 				{#if fiftyFifty.maara>2}
 				<img
 				src="cardBack.png"
-				class="mx-auto flex h-90 w-60 -translate-x-[300%] motion-safe:animate-[bounce_5s_infinite] transition duration-175 ease-in-out hover:scale-101 rounded-xl border-2 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700]"
+				class="absolute left-1/2 top-0 rounded-xl border-4 border-black outline-1 outline-[#FFD700] shadow-lg hover:shadow-[#FFD700] z-30 transform -translate-x-1/2 transition duration-300 ease-in-out hover:scale-101 object-contain motion-safe:animate-[bounce_5s_infinite]"
 				alt="Kortti"
 				onclick={kortinNaytto}
 				in:fade
@@ -140,7 +147,7 @@ nostetut.tyhjenna()
 			/>
 				{/if}
       </div>
-    </div>
+      </div>
           <!--/BOUNCY KORTIT-->
 
 
@@ -148,7 +155,7 @@ nostetut.tyhjenna()
 				<!-- <div class="mx-auto flex h-20 w-20 items-center justify-center rounded-full p-5 text-white"> -->
 					<!-- {maara} -->
 				<!-- </div> -->
-        <div class="flex justify-center">
+        <div class="w-16 sm:w-20">
 				<Button onclick={() => fiftyFifty.maara++} text="/images/plus.png" disabled={fiftyFifty.maara >= 3} />
         </div>
 
