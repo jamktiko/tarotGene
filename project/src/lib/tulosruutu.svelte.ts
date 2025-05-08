04fe35e4d7a1f94b7f7d0278f85bb34682567355
Thomas, Vainikainen <ag2982@student.jamk.fi>
2025-05-08 14:38:09 +0300

    kommenttia

--- project/src/lib/tulosruutu.svelte.ts
+++ project/src/lib/tulosruutu.svelte.ts
@@ -1,14 +1,14 @@
 class Ruutu {
-	public booleani: boolean = $state(false);
-	public maara: number = $state(1);
+	public booleani: boolean = $state(false); //Säätää etusivulla tapahtuvaa sivunvaihtoa
+	public maara: number = $state(1); //Säätää nostettujen korttien määrää
 
 	get nVaihto() {
 		return this.booleani;
-	}
+	} // Käytetty bugikorjauksessa
 
 	vaihda() {
 		this.booleani = !this.booleani;
-	}
+	} //Vaihtaa booleanin tilaa
 }
 
 const fiftyFifty = new Ruutu();
--- project/src/lib/valitutkortit.svelte.ts
+++ project/src/lib/valitutkortit.svelte.ts
@@ -1,19 +1,19 @@
 import type { Kortit } from '$lib/types/Kortit';
 
 class Valitut {
-	public nostetut: Kortit[] = $state([]);
-	public valittuAihe: number = $state(0);
+	public nostetut: Kortit[] = $state([]); //Nostettujen korttien taulukko
+	public valittuAihe: number = $state(0); //Hallitsee valittua aihetta
 	get nNostetut() {
 		return this.nostetut;
-	}
+	} //Käytetty bugikorjauksessa
 
 	tyhjenna() {
 		this.nostetut = [];
-	}
+	} // Tyhjentää nostetut taulukon
 
 	tyonna(kortti: Kortit) {
 		this.nostetut.push(kortti);
-	}
+	} //työntää nostetun kortin nostetut pakkaan
 }
 
 const nostetut = new Valitut();
